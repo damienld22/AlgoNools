591f59e78f7cb2a182ae889fd3027b909a475396
damienld22 <damien-le-dantec@hotmail.fr>
2016-02-13 15:07:09 +0100

    Ajout des premiers fichiers

---
+++ Functions.js
@@ -0,0 +1,5 @@
+// Fonction externe pour tester si on peut prendre
+// des fonctions qui nous sont pas propres (API)
+function afficher(valeur){
+	console.log(valeur);
+}
---
+++ ProjetTest.js
@@ -0,0 +1,13 @@
+// Correspondance entre nools et ce fichier JS
+var nools = require ('nools');
+var ruleFilePath = __dirname + '/ProjetTest.nools';
+var flow = nools.compile(ruleFilePath);
+var session = flow.getSession();
+
+// Création d'un objet chauffage
+var Chauffage = flow.getDefined('chauffage');
+session.assert(new Chauffage (10));
+
+// Démarrage du raisonneur
+session.match();
+
