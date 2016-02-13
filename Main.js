7840236c61d777f351312a58a25bbaf4fe52ba89
damienld22 <damien-le-dantec@hotmail.fr>
2016-02-13 22:21:01 +0100

    Instanciation de tous les équipements

--- Main.js
+++ Main.js
@@ -4,9 +4,28 @@ var ruleFilePath = __dirname + '/Main_raisonneur.nools';
 var flow = nools.compile(ruleFilePath);
 var session = flow.getSession();
 
-// Création d'un objet chauffage
+// Création d'un chauffage
 var Chauffage = flow.getDefined('chauffage');
 session.assert(new Chauffage (true, 10));
+// Création d'une lumière
+var Lumiere = flow.getDefined('lumiere');
+session.assert(new Lumiere (50));
+// Création d'un capteur de température extérieur
+var CaptTemperature = flow.getDefined('captTemperature');
+session.assert(new CaptTemperature('exterieur', true, 20));
+// Création d'un capteur de température intérieur
+var CaptTemperature = flow.getDefined('captTemperature');
+session.assert(new CaptTemperature('interieur', true, 18));
+// Création d'un capteur de luminosité extérieur
+var CaptLuminosite = flow.getDefined('captLuminosite');
+session.assert(new CaptLuminosite('exterieur', true, 400));
+// Création d'un capteur de luminosité intérieur
+var CaptLuminosite = flow.getDefined('captLuminosite');
+session.assert(new CaptLuminosite('interieur', true, 500));
+// Création d'un volet
+var Volet = flow.getDefined('volet');
+session.assert(new Volet(0));
+
 
 // Démarrage du raisonneur
 session.match();
